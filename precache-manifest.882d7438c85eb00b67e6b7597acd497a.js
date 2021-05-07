677dae1644465ceb3014f389d5affb1852bb39d7
Coder-XO <coderless@gmail.com>
2021-05-07 22:32:42 +0800

    deploy

---
+++ precache-manifest.882d7438c85eb00b67e6b7597acd497a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e887878d32c25a2498c",
+    "url": "/Money_notes_website/css/app.c78be285.css"
+  },
+  {
+    "revision": "dfe6717f4a873379c166d7faa82df2dd",
+    "url": "/Money_notes_website/img/qrcode.png"
+  },
+  {
+    "revision": "4cbb48b2a7d6f9a8b13b78b3acac8910",
+    "url": "/Money_notes_website/index.html"
+  },
+  {
+    "revision": "5e887878d32c25a2498c",
+    "url": "/Money_notes_website/js/app.3b2975dc.js"
+  },
+  {
+    "revision": "d7384e181b1110252c11",
+    "url": "/Money_notes_website/js/chunk-vendors.774ce313.js"
+  },
+  {
+    "revision": "a943af865ebf7f44344349a597479d7d",
+    "url": "/Money_notes_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money_notes_website/robots.txt"
+  }
+]);
